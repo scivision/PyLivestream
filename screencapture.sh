da40d34f0e4d755c80a0a0b43aac43ddebcf2aca
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-03 01:11:18 -0500

    init

---
+++ screencapture.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -u
+
+outfn=$1
+
+ffmpeg \
+-video_size 1024x720 \
+-framerate 10 \
+-f x11grab -i :0.0+100,200 \
+-f pulse -ac 2 -i default \
+-acodec libmp3lame -ar 48000 \
+$outfn
