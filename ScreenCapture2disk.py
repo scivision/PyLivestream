6fdf16e67385b6d13c4f0a4f73b6852dfc5ee313
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-19 14:11:41 -0500

    fix missing parameter

--- ScreenCapture2disk.py
+++ ScreenCapture2disk.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
          'res': p.res,
          'origin':p.origin,
          'audiochan': 'default',
+         'vidsource': 'screen',
             }
 
     disksave4youtube(P, p.outfn)
