c706b2843c8f0ad65a7f24043e24ba6c3a8a21ea
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-20 12:37:04 -0500

    allow looping globbed file list

--- FileGlob2YouTubeLive.py
+++ FileGlob2YouTubeLive.py
@@ -9,6 +9,13 @@ from pathlib import Path
 from getpass import getpass
 from youtubelive_ffmpeg import youtubelive
 
+def playonce(flist:list, P:dict):
+
+    for f in flist:
+        P['filein'] = f
+        youtubelive(P)
+
+
 if __name__ == '__main__':
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -18,6 +25,7 @@ if __name__ == '__main__':
     p.add_argument('path',help='path to discover files from')
     p.add_argument('glob',help='file glob pattern for YouTube Live.  Keep in mind copyright and TOS!')
     p.add_argument('-image',help='static image to display, typically used for audio-only files.')
+    p.add_argument('-loop',help='repeat the globbed file list endlessly',action='store_true')
     p = p.parse_args()
 
     path = Path(p.path).expanduser()
@@ -29,12 +37,13 @@ if __name__ == '__main__':
     print('streaming to YouTube Live these files. Be sure list is correct! \n')
     print('\n'.join([str(f) for f in flist]))
 
-    streamid = getpass('YouTube Live Stream ID: ')
-    for f in flist:
-        P = {'filein': f,
-             'vidsource': 'file',
-             'image':p.image,
-             'streamid':streamid,
-                }
+    P = {'vidsource': 'file',
+         'image':p.image,
+         'streamid':getpass('YouTube Live Stream ID: '),
+         }
 
-        youtubelive(P)
+    if p.loop:
+        while True:
+            playonce(flist, P)
+    else:
+        playonce(flist, P)
