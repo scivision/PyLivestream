d3ce528a323a95950ef8714bcb3c0edaecd23a1e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-10 21:32:40 -0500

    unittest

--- tests/Test.py
+++ tests/Test.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python
 from pathlib import Path
 import os
+import unittest
 import PyLivestream
 
 rdir = Path(__file__).parent
 os.chdir(str(rdir))
 
 inifn =  'test.ini'
-
 sites = ['periscope','youtube','facebook','twitch','mixer','ustream','vimeo']
 
-def test_screenshare():
-    for s in sites:
-        try:
-            PyLivestream.Screenshare(inifn, s)
-        except FileNotFoundError:
-            pass
+class BasicTests(unittest.TestCase):
+    def test_screenshare(self):
+        for s in sites:
+            try:
+                PyLivestream.Screenshare(inifn, s)
+            except FileNotFoundError:
+                pass
 
-def test_webcam():
-    for s in sites:
-        try:
-            PyLivestream.Webcam(inifn, s)
-        except FileNotFoundError:
-            pass
+    def test_webcam(self):
+        for s in sites:
+            try:
+                PyLivestream.Webcam(inifn, s)
+            except FileNotFoundError:
+                pass
 
 
-def test_loop():
-    for s in sites:
-        try:
-            PyLivestream.FileIn(inifn,s, 'star_collapse_out.avi')
-        except FileNotFoundError:
-            pass
+    def test_loop(self):
+        for s in sites:
+            try:
+                PyLivestream.FileIn(inifn,s, 'star_collapse_out.avi')
+            except FileNotFoundError:
+                pass
 
 
 if __name__ == '__main__':
-    test_screenshare()
-
-    test_webcam()
-
-    test_loop()
+    unittest.main()
