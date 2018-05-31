c584483ebd4bdb5be5f9fd22ae5105b3d0ae8935
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-05-31 12:16:04 -0400

    doc

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require: List[str] = ['pytest', 'nose', 'coveralls', 'flake8', 'mypy']
 
 setup(name='PyLivestream',
       packages=find_packages(),
-      version='1.6.4',
+      version='1.6.5',
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/PyLivestream',
       description=('Easy streaming using FFmpeg to '
