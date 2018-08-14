6d9dc5264aa3e0fb1cea2de47255ee84644ff6b4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-14 12:34:16 -0400

    remove vestigal option

--- visual_tests.py
+++ visual_tests.py
@@ -26,40 +26,32 @@ def main():
 
     # %% Microphone
     print('PyLivestream splash with live microphone audio')
-    subprocess.check_call(['MicrophoneLivestream', '-y',  HOST, '-image', str(STATIC)],
-                          cwd=R)
+    subprocess.check_call(['MicrophoneLivestream', '-y',  HOST, '-image', str(STATIC)])
 
     print('1990s vector graphics with live microphone audio')
-    subprocess.check_call(['MicrophoneLivestream', '-y',  HOST, '-image', str(MOVING)],
-                          cwd=R)
+    subprocess.check_call(['MicrophoneLivestream', '-y',  HOST, '-image', str(MOVING)])
     # %%  Music
     print('PyLivestream splash with orchestra music  (caption)')
     subprocess.check_call(['FileGlobLivestream', '-y',  '-image', str(STATIC),
                            str(VIDPATH), HOST,
-                           '-glob', 'orch.ogg'],
-                          cwd=R)
+                           '-glob', 'orch.ogg'])
 
     print('1990s vector graphics with orchestra music (NO caption')
     subprocess.check_call(['FileGlobLivestream', '-y', '-image', str(MOVING),
-                           str(MUSIC), HOST],
-                          cwd=R)
+                           str(MUSIC), HOST])
     # video
     print('Looping video')
     subprocess.check_call(['FileGlobLivestream', '-y',
-                           str(VIDEO), HOST],
-                          cwd=R)
+                           str(VIDEO), HOST])
     # %% Screenshare
     print('Screenshare + microphone')
-    subprocess.check_call(['ScreenshareLivestream', '-y',  HOST],
-                          cwd=R)
+    subprocess.check_call(['ScreenshareLivestream', '-y',  HOST])
     # %% loop video
     print('Looping video')
-    subprocess.check_call(['FileLoopLivestream', '-y',  str(VIDEO), HOST],
-                          cwd=R)
+    subprocess.check_call(['FileLoopLivestream', '-y',  str(VIDEO), HOST])
     # %% Webcam
     print('Webcam test - will fail if no webcam present on your system')
-    subprocess.check_call(['WebcamLivestream', '-y', HOST],
-                          cwd=R)
+    subprocess.check_call(['WebcamLivestream', '-y', HOST])
 
 
 if __name__ == '__main__':
