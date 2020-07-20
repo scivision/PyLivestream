113e3e0b7565945675cb428bf09cafa01fd83786
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-20 03:47:49 -0400

    python 3.7 required

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 import site
 import setuptools
-import sys
 
 # PEP517 workaround
 site.ENABLE_USER_SITE = True
 
-# Python 3.6 workaround
-install_requires: list = []
-if sys.version_info < (3, 7):
-    install_requires.append("importlib-resources")
-
-setuptools.setup(install_requires=install_requires)
+setuptools.setup()
